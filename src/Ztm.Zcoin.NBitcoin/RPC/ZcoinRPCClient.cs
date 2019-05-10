4d0657bf505d6d649e44bea6c76211b7fd8812c6
Putta Khunchalee <putta@zcoin.io>
2019-05-10 19:29:03 +0700

    Add GetBlockAsync() and GetBlockHeaderAsync() to IZcoinRpcClient

--- src/Ztm.Zcoin.NBitcoin/RPC/ZcoinRPCClient.cs
+++ src/Ztm.Zcoin.NBitcoin/RPC/ZcoinRPCClient.cs
@@ -7,6 +7,11 @@ namespace Ztm.Zcoin.NBitcoin.RPC
 {
     public class ZcoinRPCClient : RPCClient
     {
+        public ZcoinRPCClient(RPCCredentialString credentials, Uri address, Network network = null)
+            : base(credentials, address, network)
+        {
+        }
+
         public ZcoinRPCClient(string authenticationString, Uri address, Network network = null)
             : base(authenticationString, address, network)
         {
--- src/Ztm.Zcoin.Rpc/IZcoinRpcClient.cs
+++ src/Ztm.Zcoin.Rpc/IZcoinRpcClient.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using NBitcoin;
 using NBitcoin.RPC;
+using Ztm.Zcoin.NBitcoin;
 
 namespace Ztm.Zcoin.Rpc
 {
     public interface IZcoinRpcClient : IDisposable
     {
+        Task<ZcoinBlock> GetBlockAsync(uint256 blockHash, CancellationToken cancellationToken);
+
+        Task<ZcoinBlock> GetBlockAsync(int height, CancellationToken cancellationToken);
+
+        Task<ZcoinBlockHeader> GetBlockHeaderAsync(uint256 blockHash, CancellationToken cancellationToken);
+
+        Task<ZcoinBlockHeader> GetBlockHeaderAsync(int height, CancellationToken cancellationToken);
+
         Task<BlockchainInfo> GetBlockchainInfoAsync(CancellationToken cancellationToken);
     }
 }
