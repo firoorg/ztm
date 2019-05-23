f23b5ce6e67e64eb137eed6957d406cd33eca877
Putta Khunchalee <putta@zcoin.io>
2019-05-23 16:07:19 +0700

    Add IQueryableExtensions

---
+++ src/Ztm.Data.Entity/Contexts/IQueryableExtensions.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Ztm.Data.Entity.Contexts.Main;
+
+namespace Ztm.Data.Entity.Contexts
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<Block> IncludeAll(this IQueryable<Block> query)
+        {
+            return query.Include(e => e.Transactions)
+                .ThenInclude(e => e.Transaction)
+                .ThenInclude(e => e.Outputs)
+                .ThenInclude(e => e.Inputs)
+                .Include(e => e.Transactions)
+                .ThenInclude(e => e.Transaction)
+                .ThenInclude(e => e.Inputs)
+                .ThenInclude(e => e.Output);
+        }
+    }
+}
