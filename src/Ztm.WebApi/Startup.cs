fa932f1bfa1ffcd2b0c9c197679b458e7623c929
Putta Khunchalee <putta@zcoin.io>
2019-05-31 16:30:10 +0700

    Start BlocksSynchronizer

--- src/Ztm.WebApi/Startup.cs
+++ src/Ztm.WebApi/Startup.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NBitcoin.RPC;
+using Ztm.Configuration;
+using Ztm.Data.Entity.Contexts;
+using Ztm.Data.Entity.Postgres;
+using Ztm.Zcoin.Rpc;
+using Ztm.Zcoin.Synchronization;
 
 namespace Ztm.WebApi
 {
@@ -17,7 +24,20 @@ namespace Ztm.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // ASP.NET Services.
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Database Services.
+            services.AddSingleton<IMainDatabaseFactory, MainDatabaseFactory>();
+
+            // Zcoin Interface Services.
+            services.AddSingleton<IZcoinRpcClientFactory>(CreateZcoinRpcClientFactory);
+            services.AddTransient<IBlocksRetriever, BlocksRetriever>();
+            services.AddSingleton<IBlocksStorage, BlocksStorage>();
+
+            // Background Services.
+            services.AddTransient<IBlocksSynchronizer, BlocksSynchronizer>();
+            services.AddHostedService<ServiceManager>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +53,17 @@ namespace Ztm.WebApi
             }
 
             app.UseMvc();
+        }
+
+        IZcoinRpcClientFactory CreateZcoinRpcClientFactory(IServiceProvider provider)
+        {
+            var config = Configuration.GetZcoinSection();
+
+            return new ZcoinRpcClientFactory(
+                config.Rpc.Address,
+                config.Network.Type,
+                RPCCredentialString.Parse($"{config.Rpc.UserName}:{config.Rpc.Password}")
+            );
         }
     }
 }
