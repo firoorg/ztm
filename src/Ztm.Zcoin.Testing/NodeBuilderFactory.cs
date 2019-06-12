b00eb5d1ca2afd09b8e9f020d8fb6cd1fb94e466
Putta Khunchalee <putta@zcoin.io>
2019-06-12 15:54:49 +0700

    Upgrade Zcoin binary to 0.13.7.10

--- src/Ztm.Zcoin.Testing/NodeBuilderFactory.cs
+++ src/Ztm.Zcoin.Testing/NodeBuilderFactory.cs
@@ -10,21 +10,21 @@ namespace Ztm.Zcoin.Testing
         {
             var data = new NodeDownloadData()
             {
-                Version = "0.13.7.7",
+                Version = "0.13.7.10",
                 RegtestFolderName = "regtest",
                 Linux = new NodeOSDownloadData()
                 {
                     Archive = "zcoin-{0}-linux64.tar.gz",
                     DownloadLink = "https://github.com/zcoinofficial/zcoin/releases/download/v{0}/zcoin-{0}-linux64.tar.gz",
                     Executable = "zcoin-0.13.7/bin/zcoind",
-                    Hash = "efcb949f55ec4f1b2764aacccb12420ad22d9b759d4b4554d682563bcb4792d4"
+                    Hash = "04b11d4bed070c0131e3b546e5ebdddf174121dd6f39cc02f7f241bc56fb3a8c"
                 },
                 Windows = new NodeOSDownloadData()
                 {
                     Archive = "zcoin-{0}-win64.zip",
                     DownloadLink = "https://github.com/zcoinofficial/zcoin/releases/download/v{0}/zcoin-{0}-win64.zip",
                     Executable = "zcoin-0.13.7/bin/zcoind.exe",
-                    Hash = "6b047f8485f906d32d7ccbfd84371510d9648792d9824ccf0598acdb0bcdfb7d"
+                    Hash = "3f9b49b7eb800deeaa26158dc1f11c7488d6c03e8c20c35f9dac788cc78ce13b"
                 }
             };
 
