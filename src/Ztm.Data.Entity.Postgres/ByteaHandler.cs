0cc4534c486995750ab157c74623c07b00df1f62
thebevrishot <panu.suk@satang.com>
2019-07-23 16:59:08 +0700

    Fix uint256 endianness

--- src/Ztm.Data.Entity.Postgres/ByteaHandler.cs
+++ src/Ztm.Data.Entity.Postgres/ByteaHandler.cs
@@ -9,26 +9,19 @@ namespace Ztm.Data.Entity.Postgres
 {
     class ByteaHandler : Npgsql.TypeHandlers.ByteaHandler, INpgsqlTypeHandler<uint256>
     {
-        async ValueTask<uint256> INpgsqlTypeHandler<uint256>.Read(NpgsqlReadBuffer buf, int len, bool async, FieldDescription fieldDescription)
+        async ValueTask<uint256> INpgsqlTypeHandler<uint256>.Read(NpgsqlReadBuffer buf, int len, bool async,
+            FieldDescription fieldDescription)
         {
-            try
-            {
-                var bytes = new byte[len];
-                await buf.ReadBytes(bytes, 0, len, async);
-                return new uint256(bytes, false);
-            }
-            catch (Exception e)
-            {
-                throw new NpgsqlSafeReadException(e);
-            }
+            var bytes = await Read<byte[]>(buf, len, async, fieldDescription);
+            return new uint256(bytes);
         }
 
-        public async Task Write(uint256 value, NpgsqlWriteBuffer buf, NpgsqlLengthCache lengthCache, NpgsqlParameter parameter, bool async)
-        {
-            await buf.WriteBytesRaw(value.ToBytes(false), async);
-        }
+        async Task INpgsqlTypeHandler<uint256>.Write(uint256 value, NpgsqlWriteBuffer buf,
+            NpgsqlLengthCache lengthCache, NpgsqlParameter parameter, bool async)
+            => await Write(value.ToBytes(), buf, lengthCache, parameter, async);
 
-        int INpgsqlTypeHandler<uint256>.ValidateAndGetLength(uint256 value, ref NpgsqlLengthCache lengthCache, NpgsqlParameter parameter)
+        int INpgsqlTypeHandler<uint256>.ValidateAndGetLength(uint256 value, ref NpgsqlLengthCache lengthCache,
+            NpgsqlParameter parameter)
             => 32;
     }
 }
