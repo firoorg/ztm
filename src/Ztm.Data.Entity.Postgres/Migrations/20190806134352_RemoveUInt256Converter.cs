ce5422010c0fdcd6daf554a6142430caadc6f94c
Putta Khunchalee <putta@zcoin.io>
2019-08-06 20:49:04 +0700

    Add missing migration for uint256 optimization

---
+++ src/Ztm.Data.Entity.Postgres/Migrations/20190806134352_RemoveUInt256Converter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ztm.Data.Entity.Postgres.Migrations
+{
+    public partial class RemoveUInt256Converter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
