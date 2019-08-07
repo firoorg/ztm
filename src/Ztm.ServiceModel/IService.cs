37dae45676983905f774a6c89de4ef2b07a4b3a3
Putta Khunchalee <putta@zcoin.io>
2019-08-07 18:55:28 +0700

    Make Ztm.ServiceModel.IService not support restarting

--- src/Ztm.ServiceModel/IService.cs
+++ src/Ztm.ServiceModel/IService.cs
@@ -3,6 +3,12 @@ using Ztm.ObjectModel;
 
 namespace Ztm.ServiceModel
 {
+    /// <summary>
+    /// Represent a service.
+    /// </summary>
+    /// <remarks>
+    /// Once a service instance is stopped is cannot start anymore.
+    /// </remarks>
     public interface IService : IDisposable
     {
         Exception Exception { get; }
