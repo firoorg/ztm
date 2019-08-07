0da85d882b47ac236b6a0eb6d9676771783f1087
thebevrishot <panu.suk@satang.com>
2019-08-07 15:26:55 +0700

    Fix too verbose log on test

--- src/Ztm.Zcoin.NBitcoin.Tests/RpcTests.cs
+++ src/Ztm.Zcoin.NBitcoin.Tests/RpcTests.cs
@@ -17,7 +17,9 @@ namespace Ztm.Zcoin.NBitcoin.Tests
         public RpcTests()
         {
             this.builder = NodeBuilderFactory.CreateNodeBuilder(GetType().Name);
-            this.node = this.builder.CreateNode(true);
+            this.node = this.builder.CreateNode(false);
+            this.node.ConfigParameters.Add("printtoconsole", "0");
+            this.node.StartAsync().Wait();
             this.rpc = new ZcoinRPCClient(node.GetRPCAuth(), node.RPCUri, node.Network); node.CreateRPCClient();
         }
 
