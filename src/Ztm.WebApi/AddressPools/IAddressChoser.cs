d32634f46fe43eaa14e4d6ccb20591b1433ad70c
thebevrishot <panu.suk@satang.com>
2019-11-27 17:22:53 +0700

    Change remark tag to summary

--- src/Ztm.WebApi/AddressPools/IAddressChoser.cs
+++ src/Ztm.WebApi/AddressPools/IAddressChoser.cs
@@ -1,13 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Ztm.WebApi.AddressPools
 {
     public interface IAddressChoser
     {
-        /// <remarks>
-        /// Garantee all receiving addresses that are send to this function will be available and the set is not empty.
-        /// </remarks>
+        /// <summary>
+        /// Choose an address from addresses set.
+        /// The set could not be null or empty.
+        /// </summary>
         ReceivingAddress Choose(IEnumerable<ReceivingAddress> addresses);
     }
 }
