223ff5553bfba20c05b94d54742f29a4e21223ba
thebevrishot <panu.suk@satang.com>
2019-12-19 18:17:51 +0700

    Add block model test

--- src/Ztm.Data.Entity.Tests/Contexts/Main/BlockTests.cs
+++ src/Ztm.Data.Entity.Tests/Contexts/Main/BlockTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Ztm.Data.Entity.Contexts.Main;
+using uint256 = NBitcoin.uint256;
 
 namespace Ztm.Data.Entity.Tests.Contexts.Main
 {
@@ -11,7 +12,8 @@ namespace Ztm.Data.Entity.Tests.Contexts.Main
         {
             this.subject = new Block()
             {
-                Height = 1
+                Height = 1,
+                Hash = uint256.One
             };
         }
 
@@ -35,7 +37,8 @@ namespace Ztm.Data.Entity.Tests.Contexts.Main
         {
             var result = this.subject.CompareTo(new Block()
             {
-                Height = 2
+                Height = 2,
+                Hash = this.subject.Hash
             });
 
             Assert.True(result < 0);
@@ -46,21 +49,150 @@ namespace Ztm.Data.Entity.Tests.Contexts.Main
         {
             var result = this.subject.CompareTo(new Block()
             {
-                Height = 0
+                Height = 0,
+                Hash = this.subject.Hash
             });
 
             Assert.True(result > 0);
         }
 
         [Fact]
-        public void CompareTo_OtherHeightIsEqual_ShouldEqual()
+        public void CompareTo_OtherSameHeightAndHashIsLower_ShouldGreater()
         {
             var result = this.subject.CompareTo(new Block()
             {
-                Height = 1
+                Height = 1,
+                Hash = uint256.Zero
+            });
+
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void CompareTo_OtherSameHeightAndHashIsGreater_ShouldLower()
+        {
+            var result = this.subject.CompareTo(new Block()
+            {
+                Height = 1,
+                Hash = uint256.Parse("0000000000000000000000000000000000000000000000000000000000000002")
+            });
+
+            Assert.True(result < 0);
+        }
+
+        [Fact]
+        public void CompareTo_OtherSameHeightAndSameHash_ShouldEqual()
+        {
+            var result = this.subject.CompareTo(new Block()
+            {
+                Height = this.subject.Height,
+                Hash = this.subject.Hash
             });
 
             Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void CompareTo_OtherSameHeightAndHashIsNull_ShouldLower()
+        {
+            var subject = new Block
+            {
+                Height = 1,
+                Hash = null
+            };
+
+            var result = subject.CompareTo(new Block()
+            {
+                Height = 1,
+                Hash = uint256.Zero
+            });
+
+            Assert.True(result < 0);
+        }
+
+        [Fact]
+        public void Equals_WithNull_ShouldReturnFalse()
+        {
+            Assert.False(this.subject.Equals(null));
+        }
+
+        [Fact]
+        public void Equals_WithOtherType_ShouldReturnFalse()
+        {
+            Assert.False(this.subject.Equals(string.Empty));
+        }
+
+        [Fact]
+        public void Equals_WithDifferenceValue_ShouldReturnFalse()
+        {
+            Block other;
+
+            other = new Block
+            {
+                Height = 2,
+                Hash = this.subject.Hash
+            };
+
+            Assert.False(this.subject.Equals(other));
+
+            other = new Block
+            {
+                Height = this.subject.Height,
+                Hash = uint256.Zero
+            };
+
+            Assert.False(this.subject.Equals(other));
+        }
+
+        [Fact]
+        public void Equals_WithSameValue_ShouldReturnTrue()
+        {
+            var other = new Block
+            {
+                Height = this.subject.Height,
+                Hash = this.subject.Hash
+            };
+
+            Assert.True(this.subject.Equals(other));
+        }
+
+        [Fact]
+        public void GetHashCode_WithSameValue_ShouldGetSameHashCode()
+        {
+            // Arrange.
+            var other = new Block
+            {
+                Height = this.subject.Height,
+                Hash = this.subject.Hash
+            };
+
+            // Act.
+            var newHash = other.GetHashCode();
+
+            // Assert.
+            Assert.Equal(this.subject.GetHashCode(), newHash);
+        }
+
+        [Fact]
+        public void GetHashCode_WithDifferenceValue_ItIsHighlyPossibleThatGetDifferenceValues()
+        {
+            Block other;
+
+            other = new Block
+            {
+                Height = 2,
+                Hash = this.subject.Hash
+            };
+
+            Assert.NotEqual(this.subject.GetHashCode(), other.GetHashCode());
+
+            other = new Block
+            {
+                Height = this.subject.Height,
+                Hash = uint256.Zero
+            };
+
+            Assert.NotEqual(this.subject.GetHashCode(), other.GetHashCode());
         }
     }
 }
