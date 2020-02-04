cfbdfaea70d9ead218cb5ba8aa69e6429e538543
Putta Khunchalee <putta@zcoin.io>
2020-02-04 21:48:17 +0700

    Add more test cases

--- src/Ztm.WebApi.Tests/Watchers/TokenReceiving/TokenReceivingCallbackTests.cs
+++ src/Ztm.WebApi.Tests/Watchers/TokenReceiving/TokenReceivingCallbackTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using Xunit;
 using Ztm.Testing;
@@ -11,6 +12,8 @@ namespace Ztm.WebApi.Tests.Watchers.TokenReceiving
     {
         readonly Callback callback;
         readonly TokenReceivingCallback subject;
+        readonly IEnumerable<TokenReceivingCallback> equals;
+        readonly IEnumerable<TokenReceivingCallback> unequals;
 
         public TokenReceivingCallbackTests()
         {
@@ -22,6 +25,24 @@ namespace Ztm.WebApi.Tests.Watchers.TokenReceiving
                 new Uri("http://localhost/a"));
 
             this.subject = new TokenReceivingCallback(this.callback, "timeout");
+
+            this.equals = new[]
+            {
+                new TokenReceivingCallback(this.subject.Callback, this.subject.TimeoutStatus),
+            };
+
+            this.unequals = new[]
+            {
+                new TokenReceivingCallback(
+                    new Callback(
+                        Guid.NewGuid(),
+                        IPAddress.Parse("192.168.1.3"),
+                        DateTime.Now,
+                        false,
+                        new Uri("http://localhost/b")),
+                    this.subject.TimeoutStatus),
+                new TokenReceivingCallback(this.subject.Callback, ""),
+            };
         }
 
         [Fact]
@@ -47,11 +68,109 @@ namespace Ztm.WebApi.Tests.Watchers.TokenReceiving
         }
 
         [Fact]
+        public void Equality_WithSameInstance_ShoudReturnTrue()
+        {
+            var left = this.subject;
+            var right = this.subject;
+
+            Assert.True(left == right);
+        }
+
+        [Fact]
+        public void Equality_WithBothNull_ShoudReturnTrue()
+        {
+            TokenReceivingCallback left, right;
+
+            left = null;
+            right = null;
+
+            Assert.True(left == right);
+        }
+
+        [Fact]
+        public void Equality_WithLeftNull_ShoudReturnFalse()
+        {
+            Assert.False(null == this.subject);
+        }
+
+        [Fact]
+        public void Equality_WithRightNull_ShoudReturnFalse()
+        {
+            Assert.False(this.subject == null);
+        }
+
+        [Fact]
+        public void Equality_WithEquals_ShoudReturnTrue()
+        {
+            foreach (var v in this.equals)
+            {
+                Assert.True(this.subject == v);
+            }
+        }
+
+        [Fact]
+        public void Equality_WithUnequals_ShoudReturnFalse()
+        {
+            foreach (var v in this.unequals)
+            {
+                Assert.False(this.subject == v);
+            }
+        }
+
+        [Fact]
+        public void Inequality_WithSameInstance_ShoudReturnFalse()
+        {
+            var left = this.subject;
+            var right = this.subject;
+
+            Assert.False(left != right);
+        }
+
+        [Fact]
+        public void Inequality_WithBothNull_ShoudReturnFalse()
+        {
+            TokenReceivingCallback left, right;
+
+            left = null;
+            right = null;
+
+            Assert.False(left != right);
+        }
+
+        [Fact]
+        public void Inequality_WithLeftNull_ShoudReturnTrue()
+        {
+            Assert.True(null != this.subject);
+        }
+
+        [Fact]
+        public void Inequality_WithRightNull_ShoudReturnTrue()
+        {
+            Assert.True(this.subject != null);
+        }
+
+        [Fact]
+        public void Inequality_WithEquals_ShoudReturnFalse()
+        {
+            foreach (var v in this.equals)
+            {
+                Assert.False(this.subject != v);
+            }
+        }
+
+        [Fact]
+        public void Inequality_WithUnequals_ShoudReturnTrue()
+        {
+            foreach (var v in this.unequals)
+            {
+                Assert.True(this.subject != v);
+            }
+        }
+
+        [Fact]
         public void Equals_WithEqual_ShouldReturnTrue()
         {
-            var results = EqualityTesting.TestEquals(
-                this.subject,
-                s => new TokenReceivingCallback(s.Callback, s.TimeoutStatus));
+            var results = EqualityTesting.TestEquals(this.subject, this.equals);
 
             Assert.DoesNotContain(false, results);
         }
@@ -59,17 +178,7 @@ namespace Ztm.WebApi.Tests.Watchers.TokenReceiving
         [Fact]
         public void Equals_WithUnequal_ShouldReturnFalse()
         {
-            var results = EqualityTesting.TestInequal(
-                this.subject,
-                s => new TokenReceivingCallback(
-                    new Callback(
-                        Guid.NewGuid(),
-                        IPAddress.Parse("192.168.1.3"),
-                        DateTime.Now,
-                        false,
-                        new Uri("http://localhost/b")),
-                    s.TimeoutStatus),
-                s => new TokenReceivingCallback(s.Callback, ""));
+            var results = EqualityTesting.TestInequal(this.subject, this.unequals);
 
             Assert.DoesNotContain(true, results);
         }
